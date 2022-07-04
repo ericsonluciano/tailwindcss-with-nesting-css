fd2f04bd08c5045bed279f3cfd0791a8107b2d00
Ericson Luciano <ericsoncluciano@gmail.com>
2022-07-04 20:45:46 +0800

    :tada: Intial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tailwind with nesting css
+
+The goal of this is to implment css partials and nesting just like scss or less while together using tailwindcss.
+
+Someone asking for the repo.
+[https://youtu.be/hDc7BN_v0nw](Recording Video)
---
+++ components/Card.js
@@ -0,0 +1,13 @@
+
+export default function Card() {
+	return (
+    <div className="card">
+      <div className="card-header">
+        <h2>Welcome</h2>
+      </div>
+			<div className="card-content">
+				<p>Content should place here...</p>
+			</div>
+    </div>
+  );
+}
